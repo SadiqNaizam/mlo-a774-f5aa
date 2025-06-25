a272bf1a423a563eec53c1832bfa15985058cc1a
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-25 19:36:20 +0530

    Updated code files: main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,40 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-createRoot(document.getElementById("root")!).render(<App />);
+import './index.css';
+
+import App from './App';
+import { ThemeProvider } from './components/theme-provider';
+import IndexPage from './pages/index';
+import AboutPage from './pages/about';
+import ContactPage from './pages/contact';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <IndexPage />,
+      },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>
+);
